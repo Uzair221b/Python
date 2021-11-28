415e56e8fcbecdcccc344dd9e3bb177a2b3a3c42
Uzair221b <65871874+Uzair221b@users.noreply.github.com>
2021-11-29 02:06:31 +0530

    Delete calculate simple interest.py

--- calculate simple interest.py
+++
@@ -1,7 +0,0 @@
-import time
-p = int(input('enter the value of p'))
-r = int(input('enter the value of r'))
-t = int(input('enter the value of t'))
-si = p*r*t/100
-print('simple interest is',si)
-time.sleep(10)
